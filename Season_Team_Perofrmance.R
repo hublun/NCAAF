7d56674e5be179f32a3bd9ab8041e767b8e5ef1c
CJ Duan <cjduan@me.com>
2017-03-22 15:02:21 -0500

    Get Access to Rescords

--- Season_Team_Perofrmance.R
+++ Season_Team_Perofrmance.R
@@ -3,3 +3,14 @@ print(attributes(web_page_text))
 tree <- htmlTreeParse(web_page_text, useInternalNodes = TRUE, asText = TRUE, isHTML = TRUE)
 print(attributes(tree))
 print(tree)
+
+stats.tables.all <- readHTMLTable(tree)
+print(stats.tables.all)
+
+#====================== schedules and results ======================
+web_page.schedule <- getURLContent("http://www.espn.com/college-football/team/schedule/_/id/333/year/2016")
+trees.schedule <- htmlTreeParse(web_page.schedule, useInternalNodes = TRUE, asText = TRUE, isHTML = TRUE)
+schedules.tables <- readHTMLTable(trees.schedule)
+schedules.tables <- schedules.tables[[1]]
+schedules.tables$V3 # all scores with W or L
+
