16baf86b81677a37b70679a0b1de6e91bf8babb9
CJ Duan <cjduan@me.com>
2017-03-29 10:47:19 -0500

    Finding child and parent node

--- LigaStats.R
+++ LigaStats.R
@@ -9,10 +9,29 @@ club.tree <- htmlTreeParse(web_page_text, useInternalNodes = TRUE, asText = TRUE
 #print(attributes(tree))
 print(club.tree)
 #============== XPath =====select nodes anywhere in the document for the <div> tags==========
-club.divs <- xpathSApply(club.tree, "//div[@class='stat-score']")
+
+club.divs <- xpathSApply(club.tree, "//div[@class='stat-score']") #, saveXML) #save node as strings
+club.h3s <- xpathSApply(club.tree, "//h3") # get all h3 var names of interest
+
+club.h3s
+
+club.latt <- club.h3s[1]
+
+xmlParent(club.latt[[1]]) # find the parent node of largets attendance
+
 
 mhg <- club.divs[[1]]
 str(mhg)
+ 
+xmlName(mhg)
+names(mhg)  # child nodes of this node
+xmlAttrs(mhg) # all attributes
 
-scores <- xpathSApply(mhg, "//div/div/span/text()")
-scores
+
+xmlChildren(mhg)[4]
+xmlParent(mhg)[4]
+xmlValue(mhg)
+
+child.content <- xmlSApply(mhg, xmlValue) # loop over the nodes inside mhg and get the content as s string
+
+ 
