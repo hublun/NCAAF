ef1806da6c58d7c65d4e51fc0fe16ab427f88442
CJ Duan <CJ Duan>
2017-03-31 01:47:03 -0500

    club all seasons

--- LigaStats.R
+++ LigaStats.R
@@ -1,8 +1,29 @@
-club.id = 83
-club.season = 2013
+# Barcelona 83
+# Real Mardrid 86
+# AC Milan 103
 
-getStats(club.id, club.season)
+club.id = 103
+
+club.season.most.recent = 2016
+club.season.most.past = 2001
+
+
+getClubStats <- function(club.id){
+  
+club.stats <- data.frame()
+
+for (i in club.season.most.past:club.season.most.recent){ 
+ #===========================================================
+  statt <- getStats(club.id, i)
+  
+  if (dim(statt)[2] == 13) {
+    club.stats <- rbind.data.frame(club.stats, statt)
+  } 
+
+} #=========================================================
+return (club.stats)
+} #=========================================================
 
 
 
-
+write.csv(getClubStats(club.id), file = paste("Club", club.id, "_2001_2016.csv", sep = ""))
