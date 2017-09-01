2a6d002289971c27ade175805af480c19af6e674
CJ Duan <cjduan@me.com>
2017-09-01 16:27:05 -0700

    Linear Regression Baseline Model

--- LASSO_Ridge.R
+++ LASSO_Ridge.R
@@ -39,3 +39,53 @@ coef.apprx = coef(ffit, s=0.5, exact=FALSE)
 cbind2(coef.exact, coef.apprx)
 
 predict(ffit, newx=x[1:5,], type= "response", s=0.04, exact=FALSE)
+
+cvffit = cv.glmnet(x, y, type.measure = "mse", nfolds = 20)
+
+coef(cvffit, s = "lambda.min")
+predict(cvffit, newx=x[1:5,], s="lambda.min")
+
+foldid = sample(1:10, size=length(y), replace=TRUE)
+
+# choose the best alpha
+
+cv1 = cv.glmnet(x,y, foldid = foldid, alpha=1)
+cv.5 = cv.glmnet(x,y, foldid = foldid, alpha=0.5)
+cv0 = cv.glmnet(x,y, foldid = foldid, alpha=0)
+
+par(mfrow=c(2,2))
+plot(cv1);plot(cv.5);plot(cv0)
+
+
+plot(log(cv1$lambda), cv1$cvm, pch=19, col="red", xlab="log(lambda)", ylab=cv1$name)
+
+points(log(cv.5$lambda), cv.5$cvm, pch=22, col="green")
+points(log(cv0$lambda), cv0$cvm, pch=22, col="blue")
+
+legend("topleft", legend=c("alpha = 1", "alpha = 0.5","alpha = 0"), pch=16, col=c("red", "green","blue"))
+
+str(y.s)
+
+# convert sas data into numerical vector
+
+y.s = as.numeric(european_soccer_leagues$AggregatedAttendance)
+
+x.s = as.matrix(european_soccer_leagues[,4:11])
+
+# normalize variables
+
+x.s.std = scale(x.s)
+y.s.std = scale(y.s)
+
+str(x.s)
+#========== LASSO =====
+fit.s = glmnet(x.s.std, y.s.std)
+
+plot(fit.s)
+print(fit.s)
+coef(fit.s, s=0.1)
+
+cvfit.s = cv.glmnet(x.s.std, y.s.std, alpha=1)
+plot(cvfit.s)
+cvfit.s$lambda.min
+coef(cvfit.s, s="lambda.min")
