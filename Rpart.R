90452ef133fcb502ab64c590f27ba02543c94fdb
CJ Duan <cjduan@me.com>
2017-10-12 13:58:51 -0700

    CI test

--- Rpart.R
+++ Rpart.R
@@ -17,11 +17,23 @@ dev.off()
 
 str(learning.test)
 bn.hc <- hc(learning.test, score = "aic")
+pdag = iamb(learning.test)
+pdag
+dag = pdag2dag(pdag, ordering = c("A", "B", "C", "D", "E", "F"))
 bn.hc
-arcs(bn.hc) # display edges in a Graph object - Baysian Network object
-plot(bn.hc)
 
-bn.ab = gs(learning.test, blacklist = bl2)
+arcs(bn.hc) # display edges in a Graph object - Baysian Network object
+
+plot(bn.hc)
+# compute the score of the Baysian Network
+score(set.arc(bn.hc, from = "E", to = "B"), learning.test)
+# fitting the parameters of the local distributions, CP table
+fit = bn.fit(dag, learning.test)
+fit$D
+bn.fit.barchart(fit$D)
+bn.fit.dotplot(fit$D)
+
+bn.ab = gs(learning.test, blacklist = bl2) # learning with white or blacklisting
 plot(bn.ab)
 modelstring(bn.ab) # relationships among nodes
 #== blacklisting with dataframe and matrix ================
@@ -32,4 +44,18 @@ bl2 <- matrix(c("A", "B", "B", "C"), ncol = 2, byrow = TRUE, dimnames = list(NUL
 bn.soc <- hc(df.edf)
 bn.soc
 plot(bn.soc)
+
+fit.soc = bn.fit(bn.soc, df.edf, method = "bayes")
+fit.soc
+fit.soc$LWS
+bn.fit.xyplot(fit.soc$AggregatedAttendance)
+bn.fit.qqplot(fit.soc$AggregatedAttendance)
+bn.fit.histogram(fit.soc)
+
+coefficients(fit.soc$AggregatedAttendance)
+
+score(bn.soc, df.edf, type="loglik-g") # score a model
+choose.direction(bn.soc, data = df.edf, c("MAG", "MHG"), criterion = "bic-g", debug = TRUE)
+ # condictional independence test
+ci.test("MAG", "MHG", data=df.edf, test = "smc-cor")
 
