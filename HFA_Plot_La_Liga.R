505f42614fd11464625e83d7393f9fb3cfab91f3
CJ Duan <cjduan@me.com>
2018-03-30 13:40:53 -0700

    EPL Chart

--- HFA_Plot_La_Liga.R
+++ HFA_Plot_La_Liga.R
@@ -2,58 +2,58 @@ library("rstan")
 #============= Change Param Names to Team Names =================
 names(fit) # listing all papam names in the stanfit object
 
-names(fit)[483]
-names(fit)[483] <- "Bayern_Munich"
-names(fit)[484]
-names(fit)[484] <- "RB_Leipzig"
-names(fit)[485]
-names(fit)[485] <- "Borussia_Dortmund"
-names(fit)[486]
-names(fit)[486] <- "TSG_Hoffenheim"
-names(fit)[487]
-names(fit)[487] <- "FC_Cologne"
+names(fit)[501]
+names(fit)[501] <- "Chelsea"
+names(fit)[502]
+names(fit)[502] <- "Tottenham_Hotspur"
+names(fit)[503]
+names(fit)[503] <- "Manchester_City"
+names(fit)[504]
+names(fit)[504] <- "Liverpool"
+names(fit)[505]
+names(fit)[505] <- "Arsenal"
 
-names(fit)[488]
-names(fit)[488] <- "Hertha_Berlin"
-names(fit)[489]
-names(fit)[489] <- "SC_Freiburg"
-names(fit)[490]
-names(fit)[490] <- "Werder_Bremen"
-names(fit)[491]
-names(fit)[491] <- "Borussia_Monchengladbach"
-names(fit)[492]
-names(fit)[492] <- "Schalke_04"
+names(fit)[506]
+names(fit)[506] <- "Manchester_United"
+names(fit)[507]
+names(fit)[507] <- "Everton"
+names(fit)[508]
+names(fit)[508] <- "Southampton"
+names(fit)[509]
+names(fit)[509] <- "AFC_Bournemouth"
+names(fit)[510]
+names(fit)[510] <- "West_Bromwich_Albion"
 
-names(fit)[493]
-names(fit)[493] <- "Eintracht_Frankfurt"
-names(fit)[494]
-names(fit)[494] <- "Bayer_Leverkusen"
-names(fit)[495]
-names(fit)[495] <- "FC_Augsburg"
-names(fit)[496]
-names(fit)[496] <- "Hamburg_SV"
-names(fit)[497]
-names(fit)[497] <- "Mainz"
+names(fit)[511]
+names(fit)[511] <- "West_Ham_United"
+names(fit)[512]
+names(fit)[512] <- "Leicester_City"
+names(fit)[513]
+names(fit)[513] <- "Stoke_City"
+names(fit)[514]
+names(fit)[514] <- "Crystal_Palace"
+names(fit)[515]
+names(fit)[515] <- "Swansea_City"
 
-names(fit)[498]
-names(fit)[498] <- "Vfl_Wolfsburg"
-names(fit)[499]
-names(fit)[499] <- "FC_Ingolstadt_04"
-names(fit)[500]
-names(fit)[500] <- "SV_Darmstadt_98"
-names(fit)[481]
-names(fit)[481] <- "AS_Nancy_Lorraine"
-names(fit)[482]
-names(fit)[482] <- "Bastia"
+names(fit)[516]
+names(fit)[516] <- "Burnley"
+names(fit)[517]
+names(fit)[517] <- "Watford"
+names(fit)[518]
+names(fit)[518] <- "Hull_City"
+names(fit)[519]
+names(fit)[519] <- "Middlesbrough"
+names(fit)[520]
+names(fit)[520] <- "Sunderland"
 
 #==================== Plot the La_Liga Caterpillar Chart ========
 
 plot(fit, ci_level = 0.90, point_est ="mean", est_color = "#126622", show_outer_line = TRUE, outer_level = 0.95,
 
-pars=c("Bayern_Munich","RB_Leipzig", "Borussia_Dortmund","TSG_Hoffenheim", "FC_Cologne", "Hertha_Berlin", 
-       "SC_Freiburg", "Werder_Bremen", "Borussia_Monchengladbach",
-       "Schalke_04", "Eintracht_Frankfurt", "Bayer_Leverkusen", "FC_Augsburg", "Hamburg_SV", "Mainz", 
-       "Vfl_Wolfsburg", "FC_Ingolstadt_04", "SV_Darmstadt_98"), 
+pars=c("Chelsea","Tottenham_Hotspur", "Manchester_City","Liverpool", "Arsenal", "Manchester_United", 
+       "Everton", "Southampton", "AFC_Bournemouth",
+       "West_Bromwich_Albion", "West_Ham_United", "Leicester_City", "Stoke_City", "Crystal_Palace", "Swansea_City", 
+       "Burnley", "Watford", "Hull_City", "Middlesbrough", "Sunderland"), 
      
      show_density=FALSE, fill_color="#123489") + geom_vline(xintercept = 0, linetype=2) +
   
