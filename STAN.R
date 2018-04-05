36cbd268a8efb8105667143ce7f042c021b0aa76
CJ Duan <cjduan@me.com>
2018-04-04 17:17:02 -0700

    new model
    4 chains

--- STAN.R
+++ STAN.R
@@ -85,7 +85,7 @@ model{
   y2 ~ normal(mu2, 2);
 }"
 #=================================================
-mPoisson ="
+mPo1 ="
 
 data{
   int<lower = 1> N;// lower bound is 0
@@ -102,6 +102,9 @@ parameters {
   
   real <lower = 0> beta_01;  // intercept
   real <lower = 0> beta_02;  // intercept
+
+  //real mul;
+  //real muh;
 
   real <lower = 0> sigma_low;
   real <lower = 0> sigma_high;
@@ -151,8 +154,11 @@ model{
 beta_01 ~ cauchy(0, 10);
 beta_02 ~ cauchy(0, 10);
 
-sigma_high ~ cauchy(0, 5);
-sigma_low ~ cauchy(0, 5);
+//mul = 0; //uniform(-1, 1);
+//muh = 0; //uniform(-1, 1);
+
+sigma_high ~ cauchy(0, 1);
+sigma_low ~ cauchy(0, 1);
 
 
 
@@ -172,11 +178,104 @@ for (i in 1:N){
   }
 
 }"
+#=================================================
+mPo2 ="
 
+data{
+int<lower = 1> N;// lower bound is 0
+int<lower = 1> Nl; // number of first levels
+int<lower = 1> Nh; // number of higher levels
+
+int xL[N]; // team identifier
+int xH[N]; // league identifier  
+int y1[N]; 
+int y2[N];
+}
+
+parameters {
+
+real <lower = 0> alpha;  // intercept for guest scoring
+real  delta0;  // HFA for sport
+
+//real mul;
+//real muh;
+
+real <lower = 0> sigma_low;
+real <lower = 0> sigma_high;
+
+real dev1[Nh]; // devation from average between leagues guest scoring
+real delta1[Nh]; // HFA league
+
+real dev2[Nl]; // devation from average by team guest scoring
+real delta2[Nl]; // HFA team
+
+}
+
+transformed parameters{
+
+real delta_top;
+
+//real<lower=0> lambda_h1[Nh];
+//real<lower=0> lambda_h2[Nh];
+
+real<lower=0> lambda_l1[Nl];
+real<lower=0> lambda_l2[Nl];
+
+//real delta_h[Nh]; //rate differential at league level
+//real delta_l[Nl]; // rate differential at club level
+
+//delta_top = beta_01 - beta_02;
+
+
+for (i in 1:N){
+
+lambda_h1[xH[i]] = beta_01 + dev_high1[xH[i]];
+lambda_h2[xH[i]] = beta_02 + dev_high2[xH[i]];
+
+delta_h[xH[i]] =   lambda_h1[xH[i]] -   lambda_h2[xH[i]];
+
+lambda_l1[xL[i]] = lambda_h1[xH[i]] + dev_low1[xL[i]];
+lambda_l2[xL[i]] = lambda_h2[xH[i]] + dev_low2[xL[i]]; 
+
+delta_l[xL[i]] =   lambda_l1[xL[i]] -   lambda_l2[xL[i]];
+
+}
+
+}
+
+model{
+
+beta_01 ~ cauchy(0, 10);
+beta_02 ~ cauchy(0, 10);
+
+//mul = 0; //uniform(-1, 1);
+//muh = 0; //uniform(-1, 1);
+
+sigma_high ~ cauchy(0, 1);
+sigma_low ~ cauchy(0, 1);
+
+
+
+dev_high1 ~ normal(0, sigma_high);
+dev_high2 ~ normal(0, sigma_high);
+
+dev_low1 ~ normal(0, sigma_low);
+dev_low2 ~ normal(0, sigma_low);
+
+
+// likelihood 
+
+for (i in 1:N){
+
+y1[i] ~ poisson(lambda_l1[xL[i]]);
+y2[i] ~ poisson(lambda_l2[xL[i]]);
+}
+
+}"
 #===========compile model ========================
 #stanDso = stan_model(model_code = simplePoisson) 
 
-stanDso = stan_model(model_code = mPoisson) 
+stanDso = stan_model(model_code = mPo2) 
 #===============  Data Feed ======================
 
 N=length(HWS$yH)
@@ -199,8 +298,8 @@ remove(fit, stanDso)
 #========
 
 fit = sampling(object = stanDso, data=dataList, 
-               init=0, control=list(adapt_delta = 0.9),
-               chains=3, iter=999, warmup=333, thin=1)
+               init=0.1, control=list(adapt_delta = 0.9),
+               chains=4, iter=999, warmup=333, thin=1)
 
 class(fit)
 
